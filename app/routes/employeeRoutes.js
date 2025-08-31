0c5d0da5f2002c9c4a2ccaba3442ea77d980f9db
mulyatma <mulyaatmaja604@gmail.com>
2025-09-01 06:20:18 +0700

    add route getbyid, update, and delete for employee

--- app/routes/employeeRoutes.js
+++ app/routes/employeeRoutes.js
@@ -5,5 +5,8 @@ const auth = require('../middlewares/authMiddleware');
 
 router.post('/add', auth, employeeController.addEmployee);
 router.get('/', auth, employeeController.getEmployees);
+router.get('/:id', auth, employeeController.getEmployeeById);
+router.put('/:id', auth, employeeController.updateEmployee);
+router.delete('/:id', auth, employeeController.deleteEmployee);
 
 module.exports = router;
